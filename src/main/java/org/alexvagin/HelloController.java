b7dd54347e9f4a70675ccd15101252394f5ad9b9
Alexandr Vagin <alexvagin@gmail.com>
2022-10-09 15:00:01 +0300

    first commit

---
+++ src/main/java/org/alexvagin/HelloController.java
@@ -0,0 +1,12 @@
+package org.alexvagin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	public String handle() {
+		return "Hello WebFlux";
+	}
+}
---
+++ src/test/java/org/alexvagin/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package org.alexvagin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes={HelloController.class})
+class HelloControllerTest {
+	@Test
+	void contextLoads() {
+	}
+}
